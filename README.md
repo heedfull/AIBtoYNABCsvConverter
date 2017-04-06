bd12a02836e3d99f41349e83fdb86267b25063cc
Edwin Goddard <edwin.goddard@gmail.com>
2017-04-06 22:46:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#AIBtoYNABCsvConverter
+A python script to take the export file produced by Allied Irish Bank (AIB) and convert into a format that can be imported into [YNAB](https://www.youneedabudget.com) (short for You Need a Budget)
+
+##Installation
+
+To install dependencies run
+```
+pip install -r requirements.txt
+```
+
+##Running the script
+
+To run the script execute 
+```
+python script.py
+```
+
+Script relies on the file to be converted in the same directory and named `input.csv`
---
+++ script.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# create pandas dataframe by reading in the file input.csv
+df=pd.read_csv("input.csv")
+
+cols_to_keep = [' Posted Transactions Date',' Description1',' Debit Amount',' Credit Amount']
+
+# Create a new pandas dataframe with on the columns we want to keep
+df_new = df[cols_to_keep]
+
+# rename the columns
+df_new.columns = ['Date','Payee','Outflow','Inflow']
+
+# fill in the blanks with a zero
+df_new = df_new.fillna(0)
+
+# add a column to the new dataframe with a value of empty string for all rows
+# column needed for import to work
+df_new.loc[:,'Memo'] = ""
+
+# write new dataframe to csv file called output.csv
+df_new.to_csv("output.csv", index=False)
